df9b246cf411f0f9c5cdb16433c803d786cc0efc
Wolodimir <dvdent666@gmail.com>
2022-12-22 22:43:31 +0400

    Initial commit

---
+++ src/main/java/com/game/littlegame/LittleGameApplication.java
@@ -0,0 +1,13 @@
+package com.game.littlegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LittleGameApplication.class, args);
+    }
+
+}
